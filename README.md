7343ad2bb6d9c405baa8dfea783280ef93048ddd
VGLoic <vincent.genod.loic@gmail.com>
2025-08-28 16:11:51 +0200

    setup project

---
+++ README.md
@@ -0,0 +1,62 @@
+# Soko
+
+Soko is a service that helps you manage your Ethereum smart contracts releases. It provides a simple interface for pushing and pulling compilation artifacts between your local environment and the Soko cloud.
+
+## Domains
+
+### Account
+
+It represents a user account in the Soko system.
+
+The related actions are:
+- **sign up**: allows a user to create a new unverified account with a mail and a password,
+- **confirm sign up**: allows a user to confirm their email address and complete the sign-up process,
+- **generate an access token**: allows a user to generate a new short lived access token for their account.
+
+All the actions are authenticated using the email and password couple.
+
+### Project
+
+It represents a project in the Soko system, it contains a collection of smart contracts compilation artifacts. It is owned by a user account.
+
+The related actions are:
+- **create**: allows a user to create a new project,
+- **rename**: allows a user to rename an existing project,
+- **delete**: allows a user to delete a project,
+- **list compilation artifacts**: allows a user to list all artifacts for a project.
+
+All the actions are authenticated using an access token.
+
+### Artifact
+
+It represents a compilation artifact in the Soko system. An artifact belongs to a project. It is identified by a unique ID. It can also be tagged with an arbitrary label.
+
+The related actions are:
+- **push**: allows a user to push a new artifact for a project,
+- **pull**: allows a user to pull an existing artifact for a project,
+- **describe**: allows a user to get information about a specific artifact.
+
+## Local development
+
+To get started with local development, you'll need to set up your environment. Follow these steps:
+
+1. Make sure you have [Rust](https://www.rust-lang.org/tools/install) installed on your machine. Cargo version at the time of writing is 1.88.0.
+    ```bash
+    cargo --version
+    ```
+
+2. Set up the environment variables in a `.env` file, the required ones are indicated with the `REQUIRED` label.
+    ```bash
+    cp .env.example .env
+    ```
+
+3. Verify that the unit tests are running:
+    ```bash
+    cargo test
+    ```
+
+4. Verify that the integration tests are running:
+    ```bash
+    docker compose -f compose.integration.yml up
+    cargo test --test integration
+    ```
---
+++ src/main.rs
@@ -0,0 +1,113 @@
+use std::time::Duration;
+
+use axum::{
+    Router,
+    body::Body,
+    extract::{MatchedPath, Request},
+    http::Response,
+    routing::get,
+};
+use dotenvy::dotenv;
+use tokio::signal;
+use tower_http::{timeout::TimeoutLayer, trace::TraceLayer};
+use tracing::{Span, error, info, info_span};
+use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
+use uuid::Uuid;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    if let Err(err) = dotenv() {
+        if !err.not_found() {
+            let err = format!("Error while loading .env file: {err}");
+            error!(err);
+            return Err(anyhow::anyhow!(err));
+        }
+    }
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("info")),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new().route("/health", get(healthcheck)).layer((
+        TraceLayer::new_for_http()
+            .make_span_with(|request: &Request<_>| {
+                let matched_path = request
+                    .extensions()
+                    .get::<MatchedPath>()
+                    .map(MatchedPath::as_str);
+
+                let request_id = Uuid::new_v4();
+                info_span!(
+                    "http_request",
+                    method = ?request.method(),
+                    matched_path,
+                    request_id = %request_id
+                )
+            })
+            .on_response(
+                |response: &Response<Body>, latency: Duration, _span: &Span| {
+                    if response.status().is_server_error() {
+                        error!("response: {} {latency:?}", response.status())
+                    } else {
+                        info!("response: {} {latency:?}", response.status())
+                    }
+                },
+            ),
+        TimeoutLayer::new(Duration::from_secs(10)),
+    ));
+
+    let addr = "0.0.0.0:3000";
+    let listener = tokio::net::TcpListener::bind(&addr).await.map_err(|err| {
+        let err = format!("Error while binding the TCP listener to address {addr}: {err}");
+
+        error!(err);
+        anyhow::anyhow!(err)
+    })?;
+
+    info!("Successfully bind the TCP listener to address {addr}\n");
+
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .map_err(|err| {
+            let err = format!("Error while serving the routes: {err}");
+            error!(err);
+            anyhow::anyhow!(err)
+        })?;
+
+    info!("App has been gracefully shutdown");
+
+    Ok(())
+}
+
+async fn healthcheck() -> &'static str {
+    info!("Healthcheck called");
+    "OK"
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+}
