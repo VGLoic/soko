4970141012eb14d17ef9c5260f84bb820a6ee7fe
Loïc Vincent-Genod <37535451+VGLoic@users.noreply.github.com>
2025-08-29 15:16:03 +0200

    add config object, update log level configuration (#8)
    * add config object, update log level configuration
    
    * fix typo
    
    * add key in config parsing error
    
    * update config parsing
    
    * update default

---
+++ src/lib.rs
@@ -0,0 +1,79 @@
+use std::{
+    env::{self, VarError},
+    str::FromStr,
+};
+
+use axum::{Router, routing::get};
+use tracing::{Level, info};
+
+pub fn app_router() -> Router {
+    Router::new().route("/health", get(healthcheck))
+}
+
+async fn healthcheck() -> &'static str {
+    info!("Healthcheck called");
+    "OK"
+}
+
+pub struct Config {
+    pub port: u16,
+    pub log_level: Level,
+}
+
+impl Config {
+    pub fn build() -> Result<Config, anyhow::Error> {
+        let mut errors: Vec<String> = vec![];
+        let port = match parse_env_variable("PORT") {
+            Ok(v) => v.unwrap_or(3000_u16),
+            Err(e) => {
+                errors.push(e.to_string());
+                3000
+            }
+        };
+        // `LOG_LEVEL` has priority over `RUST_LOG`
+        let log_level = match parse_env_variable::<Level>("LOG_LEVEL") {
+            Ok(v) => v
+                .or_else(|| parse_env_variable::<Level>("RUST_LOG").unwrap_or(None))
+                .unwrap_or(Level::INFO),
+            Err(e) => {
+                errors.push(e.to_string());
+                Level::INFO
+            }
+        };
+        if !errors.is_empty() {
+            return Err(anyhow::anyhow!(errors.join(", ")));
+        }
+        Ok(Config { port, log_level })
+    }
+}
+
+fn parse_env_variable<T>(key: &str) -> Result<Option<T>, anyhow::Error>
+where
+    T: FromStr,
+    <T as FromStr>::Err: std::error::Error + Send + Sync + 'static,
+{
+    match read_optional_env_variable(key)? {
+        Some(v) => v.parse::<T>().map_err(anyhow::Error::from).map(|v| Some(v)),
+        None => Ok(None),
+    }
+    .map_err(|e| anyhow::anyhow!("[{key}]: {e}"))
+}
+
+fn read_optional_env_variable(key: &str) -> Result<Option<String>, anyhow::Error> {
+    match env::var(key) {
+        Ok(v) => {
+            if v.is_empty() {
+                Ok(None)
+            } else {
+                Ok(Some(v))
+            }
+        }
+        Err(e) => {
+            if e == VarError::NotPresent {
+                Ok(None)
+            } else {
+                Err(e.into())
+            }
+        }
+    }
+}
