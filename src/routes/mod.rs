a946d0976f2ba7c34017dfb46b2782efed243ef3
VGLoic <vincent.genod.loic@gmail.com>
2025-08-31 13:33:02 +0200

    add not found handler as fallback in router, add explicit status code to healthcheck

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,14 +1,20 @@
-use axum::{Json, Router, routing::get};
+use axum::{Json, Router, http::StatusCode, response::IntoResponse, routing::get};
 use serde::{Deserialize, Serialize};
 
 pub fn app_router() -> Router {
-    Router::new().route("/health", get(get_healthcheck))
+    Router::new()
+        .route("/health", get(get_healthcheck))
+        .fallback(not_found_handler)
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct GetHealthcheckResponse {
     pub ok: bool,
 }
-async fn get_healthcheck() -> Json<GetHealthcheckResponse> {
-    Json(GetHealthcheckResponse { ok: true })
+async fn get_healthcheck() -> (StatusCode, Json<GetHealthcheckResponse>) {
+    (StatusCode::OK, Json(GetHealthcheckResponse { ok: true }))
+}
+
+async fn not_found_handler() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, "Page not found")
 }
