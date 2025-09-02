bd86963601bc5d57777d357490af7568e1a48b2d
VGLoic <vincent.genod.loic@gmail.com>
2025-09-02 11:07:16 +0200

    implement dummy for Account

--- src/routes/account/model.rs
+++ src/routes/account/model.rs
@@ -24,20 +24,34 @@ impl Account {
 
 #[cfg(test)]
 mod tests {
-    use fake::{Fake, Faker, faker};
+    use chrono::Days;
+    use fake::{Dummy, Fake, Faker, faker};
+
+    impl<T> Dummy<T> for Account {
+        fn dummy_with_rng<R: fake::Rng + ?Sized>(_: &T, rng: &mut R) -> Self {
+            let created_at = faker::chrono::en::DateTimeBefore(
+                Utc::now().checked_sub_days(Days::new(2)).unwrap(),
+            )
+            .fake_with_rng(rng);
+            Account {
+                id: uuid::Uuid::new_v4(),
+                email: faker::internet::en::SafeEmail().fake_with_rng(rng),
+                // Bcrypt hash of `abcd1234efg``
+                password_hash: "$2y$10$EZGQ6TDVUAicnOu4LgVoI.kFmcbFkT9nlOXeLfnKZtJYF8YjMM3mG"
+                    .to_string(),
+                email_verified: true,
+                created_at,
+                updated_at: faker::chrono::en::DateTimeBetween(created_at, Utc::now())
+                    .fake_with_rng(rng),
+            }
+        }
+    }
 
     use super::*;
 
     #[test]
     fn test_update_password_hash() {
-        let mut account = Account {
-            id: uuid::Uuid::new_v4(),
-            email: faker::internet::en::SafeEmail().fake(),
-            password_hash: Faker.fake(),
-            email_verified: false,
-            created_at: Utc::now(),
-            updated_at: Utc::now(),
-        };
+        let mut account: Account = Faker.fake();
         let new_password_hash: String = Faker.fake();
         account.update_password_hash(new_password_hash.clone());
         assert_eq!(account.password_hash, new_password_hash);
